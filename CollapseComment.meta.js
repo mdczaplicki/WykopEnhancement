f685ddd78e29f31c25a14500552401dd1c447dfe
Marek Czaplicki <mdczaplicki@gmail.com>
2017-11-10 03:44:25 +0100

    Update CollapseComment.meta.js

--- CollapseComment.meta.js
+++ CollapseComment.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         CollapseComment
 // @namespace    https://github.com/mdczaplicki
-// @version      0.2
+// @version      0.1
 // @description  Wykop comment collapser
 // @author       You
 // @match        https://www.wykop.pl/link/
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/mdczaplicki/WykopEnhancement/master/CollapseComments.js
-// @updateURL    https://raw.githubusercontent.com/mdczaplicki/WykopEnhancement/master/CollapseComments.js
+// @updateURL    https://raw.githubusercontent.com/mdczaplicki/WykopEnhancement/master/CollapseComment.meta.js
 // ==/UserScript==

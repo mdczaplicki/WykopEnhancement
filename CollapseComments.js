c72a361e77a1419258e4122f792a18162e9009d3
Marek Czaplicki <mdczaplicki@gmail.com>
2017-11-10 03:39:36 +0100

    Update CollapseComments.js

--- CollapseComments.js
+++ CollapseComments.js
@@ -1,29 +1,43 @@
-var style = document.createElement('style');
-style.type = 'text/css';
-style.innerHTML = '.hidden { display: none; } .visible { display: list-item; }';
-document.getElementsByTagName('head')[0].appendChild(style);
+// ==UserScript==
+// @name         CollapseComment
+// @namespace    https://github.com/mdczaplicki
+// @version      0.2
+// @description  Wykop comment collapser
+// @author       You
+// @match        https://www.wykop.pl/link/
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/mdczaplicki/WykopEnhancement/master/CollapseComments.js
+// @updateURL    https://raw.githubusercontent.com/mdczaplicki/WykopEnhancement/master/CollapseComments.js
+// ==/UserScript==
 
-var allThreads = $('ul .sub');
+(function() {
+    'use strict';
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = '.hidden { display: none; } .visible { display: list-item; }';
+    document.getElementsByTagName('head')[0].appendChild(style);
 
-for (var j = 0; j < allThreads.length; ++j) {
-    var thread = allThreads[j];
-    if (thread.childElementCount >= 3) {
-        var array = Array.prototype.slice.call(thread.children).slice(2);
-        for (var i = 0; i < array.length; ++i) {
-            array[i].style.display = 'none';
-        }
-        var sElem = '<p class="more"><a href="javascript:void(0)">więcej komentarzy (' + array.length + ') <i class="fa fa-chevron-down"></i></a></p>';
-        var elem = document.createElement('li');
-        elem.innerHTML = sElem;
-        elem.onclick = function() {
-            var currentThread = this.parentElement;
-            var currentArray = Array.prototype.slice.call(currentThread.children).slice(2);
-            for (var i = 0; i < currentArray.length; ++i) {
-                currentArray[i].style.display = 'list-item';
+    var allThreads = $('ul .sub');
+
+    for (var j = 0; j < allThreads.length; ++j) {
+        var thread = allThreads[j];
+        if (thread.childElementCount >= 3) {
+            var array = Array.prototype.slice.call(thread.children).slice(2);
+            for (var i = 0; i < array.length; ++i) {
+                array[i].style.display = 'none';
             }
-            currentThread.removeChild(this);
-            alert('test');
+            var sElem = '<p class="more"><a href="javascript:void(0)">więcej komentarzy (' + array.length + ') <i class="fa fa-chevron-down"></i></a></p>';
+            var elem = document.createElement('li');
+            elem.innerHTML = sElem;
+            elem.onclick = function() {
+                var currentThread = this.parentElement;
+                var currentArray = Array.prototype.slice.call(currentThread.children).slice(2);
+                for (var i = 0; i < currentArray.length; ++i) {
+                    currentArray[i].style.display = 'list-item';
+                }
+                currentThread.removeChild(this);
+            };
+            thread.appendChild(elem);
         }
-        thread.appendChild(elem);
     }
-}
+})();

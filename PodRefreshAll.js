a6993c89822a58c4d7f013ac481e7458069cd480
Marek Czaplicki <mdczaplicki@gmail.com>
2017-11-29 19:28:43 +0100

    Create PodRefreshAll.js

---
+++ PodRefreshAll.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         PodRefreshAll
+// @namespace    https://github.com/mdczaplicki
+// @version      1.0
+// @description  Path of Diablo refresh all button
+// @author       Marek Czaplicki
+// @match        https://pathofdiablo.com/p/?account*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var pane = $('h4:contains("Offer")');
+
+    var button = $("<button></button>");
+    button.text("Refresh all!");
+    button.addClass('btn');
+    button.addClass('btn-success');
+    button.addClass('btn-xs');
+
+    button.css('float', 'right');
+
+    button.click(function() {refreshAll();});
+
+    pane.append(button);
+
+    function refreshAll() {
+        var buttons = $$('.btn.btn-success.btn-xs');
+        for (var i = 0; i < buttons.length; ++i) {
+            doRefresh(buttons[i]);
+        }
+    }
+})();
